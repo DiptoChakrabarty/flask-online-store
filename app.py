9a0730da38aa5ad09a92a48505da1f6adbb52912
DiptoChakrabarty <diptochuck123@gmail.com>
2020-08-04 11:47:09 +0530

    new packages and users

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,jsonify,request
 from flask_restful import Resource,Api
 from flask_sqlalchemy import SQLAlchemy
+import bcrypt
 
 app = Flask(__name__)
 api=Api(app)
@@ -15,16 +16,32 @@ class product(db.Model):
     name = db.Column(db.String(20),unique=True,nullable=False)
     price = db.Column(db.String(20),nullable=False)
 
+class users(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20),nullable=False,unique=True)
+    password = db.Column(db.String(20),nullable=False)
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+
+
 class Item(Resource):
     def get(self):
         data = request.get_json()
         print(data)
         name = data["name"]
         item = product.query.filter_by(name=name).first()
-        ret = {
-            "name": item.name,
-            "price": item.price
-        }
+
+        if item:
+            ret = {
+                "name": item.name,
+                "price": item.price
+            }
+        else:
+            ret = {
+                "msg": Not found
+            }
         return ret
     
     def post(self):
