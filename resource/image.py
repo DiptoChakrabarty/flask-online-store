9ea4f912ca384746ffc8a2e246c41761258a81f3
DiptoChakrabarty <diptochuck123@gmail.com>
2020-09-09 21:11:10 +0530

    image uploding

---
+++ resource/image.py
@@ -0,0 +1,23 @@
+from flask_restful import Resource
+from flask_uploads import UploadNotAllowed
+from flask import request
+from flask_jwt_extended import jwt_required,get_jwt_identity
+
+from libs import image_uploader
+from schemas.image import ImageSchema
+
+image_schema = ImageSchema()
+
+class ImageUpload(Resource):
+    @jwt_required
+    def post(self):
+        data = image_schema.load(request.files)
+        user_id = get_jwt_identity()
+        folder = f"user_{user_id}"
+        try:
+            image_path = image_uploader.save_images(data["image"],folder-folder)
+            basename =  image_uploader.get_basename(image_path)
+            return {"msg": "image uploaded {}".format(basen ame)},201
+        except UploadNotAllowed:
+            ext = image_uploader.get_extension(data["image"])
+            return {"msg": "Image format incorrect"}.400
---
+++ schemas/image.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema,fields
+from werkzeug.datastructures import FileStorage 
+
+class FileStorageField(fields.Field):
+    default_error_messages = {
+        "invalid": "Not valid image"
+    }
+    def _deserialize(self,value,attr,data) -> FileStorage:
+        if value is None:
+            return None
+        if not isinstance(value,FileStorage):
+            self.fail("invalid")
+        return value
+
+
+class ImageSchema(Schema):
+    image = FileStorageField(required=True)
