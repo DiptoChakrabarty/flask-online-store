2bc7a37164114465908353f2723a8ad08932e6d5
DiptoChakrabarty <diptochuck123@gmail.com>
2020-09-23 09:56:51 +0530

    oauth obj

---
+++ outh.py
@@ -0,0 +1,21 @@
+import os 
+from flask_oauthlib.client import OAuth
+from dotenv import load_dotenv
+load_dotenv()
+
+oauth = OAuth()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CILENT_SECRET")
+#print(client_id,client_secret)
+
+github = oauth.remote_app(
+    'github',
+    consumer_key= client_id,
+    consumer_secret = client_secret,
+    request_token_params = {"scope": "user:email"},
+    base_url="https://api.github.com/",
+    request_token_url=None,
+    access_token_method="POST",
+    access_token_url="https://github.com/login/oauth/access_token"
+)
