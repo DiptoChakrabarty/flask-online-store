3933404e801e55f9eba28ac722fd4106f5a9cf43
DiptoChakrabarty <diptochuck123@gmail.com>
2022-12-03 16:57:48 +0530

    improve gitignore and remove venv

--- venv/lib/python3.6/site-packages/stripe/api_resources/price.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-from stripe.api_resources.abstract import CreateableAPIResource
-from stripe.api_resources.abstract import ListableAPIResource
-from stripe.api_resources.abstract import UpdateableAPIResource
-
-
-class Price(CreateableAPIResource, ListableAPIResource, UpdateableAPIResource):
-    OBJECT_NAME = "price"
